41ca816b6cf046482f4792d7a83138c15423ade4
lidanqing01 <lidanqing01@baidu.com>
2014-06-18 18:17:39 +0800

    lookup encode from html

--- readTitle.pl
+++ readTitle.pl
@@ -1,153 +1,95 @@
-#!/usr/bin/perl 
-#author: tanjiti
+#!/usr/bin/perl
 use strict;
 use warnings;
 use LWP::UserAgent;
 use Getopt::Long;
 use utf8;
 use HTML::Encoding 'encoding_from_http_message';
-use Carp;
 use Encode qw(encode decode);
-use JSON;
-use List::MoreUtils qw(uniq);
-use autodie;
+use feature qw(say);
 
 binmode(STDIN, ':encoding(utf8)');
 binmode(STDOUT, ':encoding(utf8)');
 binmode(STDERR, ':encoding(utf8)');
 
 
-use Term::ANSIColor qw(:constants);
 
-local $Term::ANSIColor::AUTORESET = 1;
-
-my $file = q{};
-my $host = q{};
-my $help = q{};
-
-GetOptions(
-    'help|h'=>\$help,
-    'f=s'=>\$file,
-    'host=s'=>\$host,
-);
-
-if($help){
-    print <<__HELP__;
-Notice: In order to run this script you must have Bundle::LWP,Getopt::Long,List::MoreUtils,HTML::Encoding installed
-
-Usage: $0 -f=filename
-
-where:
--f : Specify the file include hostlist for analyze
--host : Specify the host for analyze
-
-__HELP__
-    exit 0;
-}
 
 die "You must specify the file include hostlist for analyze or the hostname
-for analyze .Please run --help for more information.\n" if ($file eq q{} and $host eq q{});
+for analyze .\n" if ($#ARGV != 0);
+my $host = shift;
 
-
-my $str = $host."\t".getResponse($host,0)."\n";
-print  $str if $host ne q{};
-
-if ($file ne q{}){
-    my @HOST = readHostFromFile($file);
-     
-    foreach (@HOST){
-       my %tmp = %$_;
-       my $title = getResponse($tmp{"host"},$tmp{"number"});
-       #print $tmp{"host"}."\t".$tmp{"number"}."\t".$title."\n";
-       print  $tmp{"host"}."\t".$title."\n";
-       
-    }
-}
-
-
-
-
-sub readHostFromFile{
-    my $file = shift;
-    my @HOST = ();
-    open FH,'<:encoding(UTF-8)',$file or die "cannot open $file for reading: $! \n";
-    while(<FH>){
-        chomp;
-        #if ($_ =~ /(\S+?)\s+?(\d+?)\z/msx){
-            my %info = ();
-            #$info{"host"} = $1;
-            #$info{"number"} = $2;
-            $info{"host"} = $_;
-            $info{"number"} = 0;
-            push(@HOST,\%info);
-        #}
-        
-    }   
-    close FH;
-    return @HOST;
-}
-
-
-sub getResponse{
-    my ($host, $number) = @_;
+if(-e $host){
     
+    #open file include hostlist
+    my $out = $host."_out";
+    open my $FH, "<:encoding(UTF-8)", $host or die "cannot open $host for reading $!";
+    open my $OUT, ">:encoding(UTF-8)", $out or die "cannot open $out for writing $!";
 
+    while(<$FH>){
+        say $OUT getTitle($_);
+
+    }
+    close $FH;
+    close $OUT;
+
+
+
+
+}else{
+    say getTitle($host) if $host ne q{};
+
+
+}
+
+
+
+sub getTitle{
+    my $uri = shift;
+
+    #set browser
+    my $UserAgent = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:12.0) Gecko/20100101 Firefox/12.0";
+    my $timeout = 180;
+    my $redirect = 30;
     my $browser = LWP::UserAgent->new();
-
-    my $UA = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:12.0) Gecko/20100101 Firefox/12.0";
+    $browser->timeout($timeout);
+    $browser->ssl_opts(verify_hostname => 1);
+    $browser->max_redirect($redirect);
     
-    $browser->agent($UA);
-    
-    $browser->timeout(10);
-
-    my $url = "http://".$host;
-    
-    my $response = $browser->get($url);
-    
-    my $status_line=$response->status_line;
-    
-    my $title = "no title";
-
-    if($response->is_success){
-        $title = readTitle($response,$host,$number) if $response->content_type
-        eq 'text/html'; 
-           
-    }
-    elsif($status_line =~  /https/){
-        $browser->ssl_opts("verify_hostname" => 1); 
-        $url = "https://".$host."\n";
-        $response = $browser->get($url);
-       
-        if($response->is_success){
-               $title = readTitle($response,$host,$number) if
-               $response->content_type eq 'text/html';
-        }
-
+    my $host = $uri;
+    if($uri =~ /https?:\/\/(\S+)/){
+      $host = $1;  
     }else{
-        $title = $status_line;
-    }
-    return $title;
+      $uri = 'http://'.$uri;
+    } 
+    $browser->default_headers->push_header('Host' => $host);
 
-}
-
-sub readTitle{
-    
-    my $title = "no title";
-
-    my ($response, $host, $number) = @_;
-
-    my $enco = encoding_from_http_message($response);
+    my $response = $browser->get($uri);
    
-         
-    my $content = decode($enco=>$response->content) or warn "cannot decode $host use $enco :  $! \n" ;
+    my $enco = $1 if ($response->decoded_content =~ /charset\s*=\s*"?([-\w]+)"?/);
+
+    my $title = $response->title;    
     
-    if (defined $content and $content =~ /<title[\s\S]*?>(.*?)<\/title>/imxs){
-        
-        $title = $1;
-        
+    if($response->is_success){
+
+        unless( $title){
+            $title = $1 if $response->decoded_content =~  /<title>(.*?)<\/title>/imxs ;
+        } 
+
+    
+    }else{
+        $title =  $response->status_line unless $title;
+    
     }
 
-    return $title;
+    $title = $enco ? decode($enco, $title) : $title;
+
+    $title = "no title" unless $title;
+
+    return $uri."\t".$title;
+    
+
 }
+
 
 
